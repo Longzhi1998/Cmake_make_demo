2d0faed440a381749bd3763d9657a63298511073
longzhi1998 <1693254825@qq.com>
2022-07-14 11:39:08 +0800

    添加了主函数

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+@describe: test of Cmake and make;
+@create date: 2022/7/14/11：25
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char **args) {
+    cout << "Test of cmake and make ! \n" << endl;
+    return 0;
+}
